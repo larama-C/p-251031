486f08ca9e8b197d1554705029bb35f012dfb95d
larama-C <ccbj0825@naver.com>
2025-10-31 11:07:42 +0900

    1단계

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+package com.back
+
+fun main() {
+
+    println("== 명언 앱==")
+
+    while (true) {
+        print("입력 : ")
+        val input = readln().trim()
+
+        if (input == "종료") {
+            break
+        }
+    }
+}
