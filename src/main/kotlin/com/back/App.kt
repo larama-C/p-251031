1cfe8505f6649db066daaacf7b84f4e31978e4f2
larama-C <ccbj0825@naver.com>
2025-10-31 11:30:32 +0900

    5단계

--- src/main/kotlin/com/back/App.kt
+++ src/main/kotlin/com/back/App.kt
@@ -3,6 +3,7 @@ package com.back
 class App {
     fun run() {
         var lastId = 0
+        val wiseSayings = mutableListOf<WiseSaying>()
 
         println("== 명언 앱 ==")
 
@@ -23,7 +24,18 @@ class App {
                     val author = readln().trim()
                     val id = ++lastId
 
+                    wiseSayings.add(WiseSaying(id, content, author))
+
                     println("${id}번 명언이 등록되었습니다.")
+                }
+
+                "목록" -> {
+                    println("번호 / 작가 / 명언")
+                    println("----------------------")
+
+                    wiseSayings.reversed().forEach {
+                        println("${it.id} / ${it.author} / ${it.content}")
+                    }
                 }
             }
         }
---
+++ src/main/kotlin/com/back/WiseSaying.kt
@@ -0,0 +1,7 @@
+package com.back
+
+data class WiseSaying(
+    val id: Int,
+    val content: String,
+    val author: String
+)
