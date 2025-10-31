c086604e39a43c7fa1e7b092cbef6385c2d62d98
larama-C <ccbj0825@naver.com>
2025-10-31 12:36:22 +0900

    firstOrNull 처리

--- src/main/kotlin/com/back/App.kt
+++ src/main/kotlin/com/back/App.kt
@@ -45,15 +45,17 @@ class App {
                         println("id를 정확히 입력해주세요.")
                         continue
                     }
-                    val removed = wiseSayings.removeIf {
+                    val wiseSaying = wiseSayings.firstOrNull {
                         it.id == id
                     }
 
-                    if(removed) {
-                        println("${id}번 명언이 삭제되었습니다.")
-                    } else {
+                    if (wiseSaying == null) {
                         println("${id}번 명언은 존재하지 않습니다.")
+                        continue
                     }
+
+                    wiseSayings.remove(wiseSaying)
+                    println("${id}번 명언이 삭제되었습니다.")
                 }
             }
         }
